891753623a28cd0cb3d301b8483a7a172edfc8d0
Luísa Braga <mluisapbraga@gmail.com>
2024-04-05 20:19:35 -0400

    idade e altura em c++

---
+++ teste.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int idade;
+    float altura;
+
+    cin >> idade;
+    cin >> altura;
+
+    cout << "Altura: " << altura  << endl;
+    cout << "Idade: " << idade  << endl;
+
+
+    return 0;
+}
