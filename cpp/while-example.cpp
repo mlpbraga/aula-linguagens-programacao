ec0baa2731d2cf784c9771c0bf71bd7fda8daa6c
Luísa Braga <mluisapbraga@gmail.com>
2024-04-09 14:27:56 -0400

    Create while-example.cpp

---
+++ cpp/while-example.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int contador = 1;
+
+    while (contador <= 5) {
+        std::cout << "Valor do contador: " << contador << std::endl;
+        contador++; // Incrementa o contador a cada iteração
+    }
+
+    return 0;
+}
