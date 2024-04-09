543cef9ad7fa55a9a256a0ad1e4f9135fb1def82
Luísa Braga <mluisapbraga@gmail.com>
2024-04-09 14:25:13 -0400

    Create input-example.cpp

---
+++ cpp/input-example.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, soma;
+
+    cin >> a;
+    cin >> b;
+
+    soma = a + b;
+
+    cout << "A soma de a + b é: " << soma <<endl;
+    return 0;
+}
