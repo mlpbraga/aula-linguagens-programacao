bac2aaeaef730d31c92200fdb01b4d4f4d76344a
Luísa Braga <mluisapbraga@gmail.com>
2024-04-09 14:20:38 -0400

    Create if-example.cpp

---
+++ cpp/if-example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, soma;
+
+    cin >> a;
+
+    if (a > 50) {
+        cout << "maior que 50" <<endl;
+    } else {
+        cout << "menor ou igual a 50" <<endl;
+    }
+
+    return 0;
+}
