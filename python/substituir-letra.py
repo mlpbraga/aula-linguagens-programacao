824dd2d8f60df99843758d4922c117f3bf7039ef
Luísa Braga <mluisapbraga@gmail.com>
2024-04-09 21:24:19 -0400

    Update substituir-letra.py

--- python/substituir-letra.py
+++ python/substituir-letra.py
@@ -12,9 +12,4 @@ nome = input("Digite o seu nome: ")
 posicao = int(input("Digite a posição em que deseja adicionar uma letra: ")) - 1  # Ajuste para base 0
 
 # Verificando e modificando a letra na posição indicada
-if nome[posicao] == 'R':
-    nome = nome[:posicao] + 'S' + nome[posicao+1:]
-elif nome[posicao] == 'M':
-    nome = nome[:posicao] + 'N' + nome[posicao+1:]
-else:
-    nome = nome[:posicao] + nome[posicao+1:]
+...
