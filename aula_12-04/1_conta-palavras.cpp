96e5f9ec4cae5bd4b2cfb2b2d4d97b5282ae6052
Luísa Braga <mluisapbraga@gmail.com>
2024-04-12 12:36:26 -0400

    Create 1_conta-palavras.cpp

---
+++ aula_12-04/1_conta-palavras.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string texto;
+    int numero_de_palavras = 0;
+
+    cout << "Digite um texto: ";
+    getline(cin, texto);
+
+    // Seu código para contar as palavras vai aqui
+
+    cout << "O número de palavras é: " << numero_de_palavras << endl;
+
+    return 0;
+}
